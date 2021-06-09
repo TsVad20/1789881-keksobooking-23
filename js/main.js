1140fa30833376c8571c505bc0389ba597b1d0ac
TsVad20 <vvts.80@gmail.com>
2021-06-09 18:19:35 +0300

    выполнил дз 4

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@
 // Учтите, что диапазон может быть только положительный, включая ноль. А также придумайте, как функция должна вести себя, если передать значение «до» меньшее,
 //чем значение «от», или равное ему.
 
-function getRandomValue(min, max) { //Источник алгоритма https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+const getRandomValue = function (min, max) { //Источник алгоритма https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
   min = Math.ceil(min);
   max = Math.floor(max);
   if (min < 0) {
@@ -35,7 +35,7 @@ getRandomValue(3, 20);
 // Не забудьте, что в случае с дробными числами диапазон может быть в десятых, сотых, тысячных и т. д. долях. Например, 1.1, 1.2 — корректный диапазон.
 
 
-function getRandomValueFloat(minValue, maxValue, numberOfDigits) {
+const getRandomValueFloat = function (minValue, maxValue, numberOfDigits) {
   minValue = minValue * Math.pow(10, numberOfDigits);
   maxValue = maxValue * Math.pow(10, numberOfDigits);
   let result;
@@ -52,4 +52,159 @@ function getRandomValueFloat(minValue, maxValue, numberOfDigits) {
   return result = result.toFixed(numberOfDigits);
 }
 getRandomValueFloat(3, 3.1, -5);
+
+/*В файле main.js на основе написанных в прошлом задании вспомогательных функций напишите необходимые функции для создания массива из 10 сгенерированных JS-объектов.
+Каждый объект массива — описание похожего объявления неподалёку.
+
+Структура каждого объекта должна быть следующей:
+
+author, объект — описывает автора. Содержит одно поле:
+
+  avatar, строка — адрес изображения вида img/avatars/user{{xx}}.png, где {{xx}} — это число от 1 до 8 с ведущим нулём. Например, 01, 02 и т. д. Адреса изображений не повторяются.
+
+offer, объект — содержит информацию об объявлении. Состоит из полей:
+
+  title, строка — заголовок предложения. Придумайте самостоятельно.
+
+  address, строка — адрес предложения. Для простоты пусть пока составляется из географических координат по маске {{location.x}}, {{location.y}}.
+
+  price, число — стоимость. Случайное целое положительное число.
+
+  type, строка — одно из пяти фиксированных значений: palace, flat, house, bungalow или hotel.
+
+  rooms, число — количество комнат. Случайное целое положительное число.
+
+  guests, число — количество гостей, которое можно разместить. Случайное целое положительное число.
+
+  checkin, строка — одно из трёх фиксированных значений: 12:00, 13:00 или 14:00.
+
+  checkout, строка — одно из трёх фиксированных значений: 12:00, 13:00 или 14:00.
+
+  features, массив строк — массив случайной длины из значений: wifi, dishwasher, parking, washer, elevator, conditioner. Значения не должны повторяться.
+
+  description, строка — описание помещения. Придумайте самостоятельно.
+
+  photos, массив строк — массив случайной длины из значений: https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg, https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg, https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg.
+
+location, объект — местоположение в виде географических координат. Состоит из двух полей:
+
+  lat, число с плавающей точкой — широта, случайное значение от 35.65000 до 35.70000.
+
+  lng, число с плавающей точкой — долгота, случайное значение от 139.70000 до 139.80000.*/
+
+/*let firstAd = {
+  author: {
+    avatar: img / avatars / user01.png,
+  },
+  offer: {
+
+    title: 'Образец обьявления', // строка — заголовок предложения. Придумайте самостоятельно.
+
+    address: { //строка — адрес предложения. Для простоты пусть пока составляется из географических координат по маске {{location.x}}, {{location.y}}.
+
+      x: location.lat,
+
+      y: location.lng,
+
+    },
+
+    price: 1000, //число — стоимость. Случайное целое положительное число.
+
+    type: palace, //строка — одно из пяти фиксированных значений: palace, flat, house, bungalow или hotel.
+
+    rooms: 3, //число — количество комнат. Случайное целое положительное число.
+
+    guests: 3, //число — количество гостей, которое можно разместить. Случайное целое положительное число.
+
+    checkin: '12:00', //строка — одно из трёх фиксированных значений: 12:00, 13:00 или 14:00.
+
+    checkout: '14:00', //, строка — одно из трёх фиксированных значений: 12:00, 13:00 или 14:00.
+
+    features: [wifi, dishwasher, parking], //массив строк — массив случайной длины из значений: wifi, dishwasher, parking, washer, elevator, conditioner. Значения не должны повторяться.
+
+    description: 'до моря 100 метров', //строка — описание помещения. Придумайте самостоятельно.
+
+    photos: [
+      'https: //assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+      'https: //assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+      'https: //assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'
+    ], //массив строк — массив случайной длины из значений: https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg, https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg, https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg.
+
+    location: { //объект — местоположение в виде географических координат. Состоит из двух полей:
+
+      lat: 35.65000, //число с плавающей точкой — широта, случайное значение от 35.65000 до 35.70000.
+
+      lng: 139.7000, //число с плавающей точкой — долгота, случайное значение от 139.70000 до 139.80000.
+
+    }
+  },
+}*/
+//переменные для свойств 'offer'
+const typeOfProperty = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
+const timeOfCheckIn = ['12:00', '13:00', '14:00'];
+const timeOfCheckOut = ['12:00', '13:00', '14:00'];
+const listOfFeatures = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+const listOfPhotos = [
+  'https: //assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https: //assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https: //assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'
+];
+// функция получения случайного значения из переданного массива
+let getRandomItem = function (exArr) {
+  return exArr[getRandomValue(0, exArr.length - 1)];
+};
+// функция получения случайного массива из элементов переданного
+let getRandomListItem = function (exArr) {
+  return exArr.slice(0, getRandomValue(0, exArr.length + 1));
+};
+// функция получения случайных координат для обьекта 'location' и 'offer.address"
+let getRandomLocation = function () {
+  return {
+    lat: getRandomValueFloat(35.65000, 35.70000, 5),
+    lng: getRandomValueFloat(139.70000, 139.80000, 5),
+  };
+};
+// присваивание координат переменным, для того, чтобы координаты 'location' и 'offer.address' были одинаковыми
+let randomLocationX = getRandomLocation().lat;
+let randomLocationY = getRandomLocation().lng;
+
+const newAd = function () {
+  return {
+
+    author: {
+      avatar: `img / avatars / user0${getRandomValue(1,8)}.png`,
+    },
+
+    location: {
+      lat: randomLocationX,
+      lng: randomLocationY,
+    },
+
+    offer: {
+
+      title: 'Образец обьявления', // строка — заголовок предложения. Придумайте самостоятельно.
+
+      address: `${randomLocationX},${randomLocationY}`, //строка — адрес предложения. Для простоты пусть пока составляется из географических координат по маске {{location.x}}, {{location.y}}.
+
+      price: getRandomValue(100, 10000), //число — стоимость. Случайное целое положительное число.
+
+      type: getRandomItem(typeOfProperty), //getRandomItem(['palace', 'flat', 'house', 'bungalow', 'hotel']), //строка — одно из пяти фиксированных значений: palace, flat, house, bungalow или hotel.
+
+      rooms: getRandomValue(1, 20), //число — количество комнат. Случайное целое положительное число.
+
+      guests: getRandomValue(1, 20), //число — количество гостей, которое можно разместить. Случайное целое положительное число.
+
+      checkin: getRandomItem(timeOfCheckIn), //строка — одно из трёх фиксированных значений: 12:00, 13:00 или 14:00.
+
+      checkout: getRandomItem(timeOfCheckOut), //, строка — одно из трёх фиксированных значений: 12:00, 13:00 или 14:00.
+
+      features: getRandomListItem(listOfFeatures), //массив строк — массив случайной длины из значений: wifi, dishwasher, parking, washer, elevator, conditioner. Значения не должны повторяться.
+
+      description: 'до моря 100 метров', //строка — описание помещения. Придумайте самостоятельно.
+
+      photos: getRandomListItem(listOfPhotos), //массив случайной длины из значений
+
+    },
+  };
+};
 
