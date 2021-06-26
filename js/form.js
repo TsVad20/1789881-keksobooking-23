941414492dd1ff7da3991b39c750645f32a53f72
TsVad20 <vvts.80@gmail.com>
2021-06-26 11:48:29 +0300

    fixed check

--- js/form.js
+++ js/form.js
@@ -8,14 +8,14 @@ export const activePage = function () {
   adForm.classList.remove('ad-form--disabled');
 
   adFormElements.forEach((element) => {
-    element.setAttribute("disabled", false);
-  })
+    element.setAttribute('disabled', false);
+  });
 
   mapFiltersForm.classList.remove('ad-form--disabled');
 
   mapFiltersFormElements.forEach((element) => {
-    element.setAttribute("disabled", false);
-  })
+    element.setAttribute('disabled', false);
+  });
 
 };
 export const unactivePage = function () {
@@ -23,13 +23,13 @@ export const unactivePage = function () {
   adForm.classList.add('ad-form--disabled');
 
   adFormElements.forEach((element) => {
-    element.setAttribute("disabled", true);
-  })
+    element.setAttribute('disabled', true);
+  });
 
   mapFiltersForm.classList.add('ad-form--disabled');
 
   mapFiltersFormElements.forEach((element) => {
-    element.setAttribute("disabled", true);
-  })
+    element.setAttribute('disabled', true);
+  });
 
 };
